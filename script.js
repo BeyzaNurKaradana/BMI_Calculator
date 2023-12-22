a0470e85a07d8e4e1b3e139feca4dd6acf52e93e
Beyza Nur Karadana <83921230+BeyzaNurKaradana@users.noreply.github.com>
2023-12-22 18:08:51 +0300

    modified js codes

--- script.js
+++ script.js
@@ -4,14 +4,30 @@ function calculate() {
   let weight = document.getElementById("weight").value;
   let bmi = weight / (height * height);
 
-  if (gender && gender.value === "female" && bmi < 18.5) {
-    // gender: Bu, cinsiyet seçeneklerinden birinin seçilip seçilmediğini kontrol eder. Eğer bir cinsiyet seçeneği seçilmişse (yani gender truthy bir değerse), bu şart sağlanır. gender.value === "female": Eğer cinsiyet seçeneği seçilmişse, bu kısmı kontrol eder. Seçilen cinsiyet "female" ise, bu şart sağlanır.
-    document.getElementById("conclusion").innerHTML = "Underweight";
-  } else if (gender && gender.value === "female" && bmi >= 18.5 && bmi < 24.9) {
-    document.getElementById("conclusion").innerHTML = "Normal weight";
-  } else if (gender && gender.value === "female" && bmi >= 25 && bmi < 29.9) {
-    document.getElementById("conclusion").innerHTML = "overweight";
-  } else {
-    document.getElementById("conclusion").innerHTML = "obese";
+ 
+
+  if (gender.value === "female") {
+    if (bmi < 18.5) {
+      document.getElementById("conclusion").innerHTML = "Underweight";
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+      document.getElementById("conclusion").innerHTML = "Normal weight";
+    } else if (bmi >= 25 && bmi < 29.9) {
+      document.getElementById("conclusion").innerHTML = "overweight";
+    } else if (bmi> 29.9) {
+      document.getElementById("conclusion").innerHTML = "obese";
+    }
+  } else if (gender.value="male") {
+    if (bmi < 19) {
+      document.getElementById("conclusion").innerHTML = "Underweight";
+    } else if (bmi >= 19 && bmi <= 25) {
+      document.getElementById("conclusion").innerHTML = "Normal weight";
+    } else if (bmi > 25 && bmi < 29.9) {
+      document.getElementById("conclusion").innerHTML = "overweight";
+    } else if (bmi> 29.9) {
+      document.getElementById("conclusion").innerHTML = "obese";
+    }
   }
+
+ 
+
 }
