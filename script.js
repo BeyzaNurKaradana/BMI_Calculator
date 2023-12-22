a83d5fc19256b1c1940c13444ff6fa99439d1a41
Beyza Nur Karadana <83921230+BeyzaNurKaradana@users.noreply.github.com>
2023-12-23 01:06:47 +0300

    added regex

--- script.js
+++ script.js
@@ -2,18 +2,19 @@ function calculate() {
   let gender = document.querySelector("input[name='gender']:checked");
   let height = document.getElementById("height").value;
   let weight = document.getElementById("weight").value;
-  
 
+
+  height = parseFloat(height) / 100;
 
   if (gender !== null && (height === "" || weight === "")) {
     alert("Please enter your height and weight!");
     document.getElementById("conclusion").innerHTML = "";
     return;
-  } else if (gender === null && (height === "" && weight !== "")) {
+  } else if (gender === null && height === "" && weight !== "") {
     alert("Please enter your gender and height!");
     document.getElementById("conclusion").innerHTML = "";
     return;
-  } else if (gender === null && (height !== "" && weight === "")) {
+  } else if (gender === null && height !== "" && weight === "") {
     alert("Please enter your gender and weight!");
     document.getElementById("conclusion").innerHTML = "";
     return;
@@ -21,32 +22,52 @@ function calculate() {
     alert("Please enter your gender!");
     document.getElementById("conclusion").innerHTML = "";
     return;
-  } 
- 
+  } else if (gender === null && height === "" && weight === "") {
+    alert("Please enter your gender height, and weight!");
+    return;
+  }
 
   let bmi = weight / (height * height);
   if (gender.value === "female") {
     if (bmi < 18.5) {
-      document.getElementById("conclusion").innerHTML = "Underweight";
+      document.getElementById("conclusion").innerHTML =
+        "Your BMI is " + bmi.toFixed(2) + "<br>You are Underweight.";
     } else if (bmi >= 18.5 && bmi < 24.9) {
-      document.getElementById("conclusion").innerHTML = "Normal weight";
+      document.getElementById("conclusion").innerHTML =
+        "Your BMI is " + bmi.toFixed(2) + "<br>You are normal weight.";
     } else if (bmi >= 25 && bmi < 29.9) {
-      document.getElementById("conclusion").innerHTML = "overweight";
-    } else if (bmi> 29.9) {
-      document.getElementById("conclusion").innerHTML = "obese";
+      /* document.getElementById("conclusion").innerHTML = "overweight"; */
+      document.getElementById("conclusion").innerHTML =
+        "Your BMI is " + bmi.toFixed(2) + "<br>You are Overweight.";
+    } else if (bmi > 29.9) {
+      document.getElementById("conclusion").innerHTML =
+        "Your BMI is " + bmi.toFixed(2) + "<br>You are Obese.";
     }
-  } else if (gender.value="male") {
+  } else if ((gender.value = "male")) {
     if (bmi < 19) {
-      document.getElementById("conclusion").innerHTML = "Underweight";
+      document.getElementById("conclusion").innerHTML =
+        "Your BMI is " + bmi.toFixed(2) + "<br>You are Underweight.";
     } else if (bmi >= 19 && bmi <= 25) {
-      document.getElementById("conclusion").innerHTML = "Normal weight";
+      document.getElementById("conclusion").innerHTML =
+        "Your BMI is " + bmi.toFixed(2) + "<br>You are normal weight.";
     } else if (bmi > 25 && bmi < 29.9) {
-      document.getElementById("conclusion").innerHTML = "overweight";
-    } else if (bmi> 29.9) {
-      document.getElementById("conclusion").innerHTML = "obese";
+      document.getElementById("conclusion").innerHTML =
+        "Your BMI is " + bmi.toFixed(2) + "<br>You are Overweight.";
+    } else if (bmi > 29.9) {
+      document.getElementById("conclusion").innerHTML =
+        "Your BMI is " + bmi.toFixed(2) + "<br>You are Obese.";
     }
   }
-
- 
-
 }
+
+initIMask = () => {
+  $("input.imask").each((n, t) => {
+    const i = $(t).data("is-regex")
+      ? new RegExp($(t).data("mask"))
+      : $(t).data("mask");
+    new IMask(t, {
+      mask: i,
+    });
+  });
+};
+
